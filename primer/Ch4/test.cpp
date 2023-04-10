2af038737745e4db3a9d5fba4054d12dd1e715ea
Matrix <faithlaysath@gmail.com>
2023-04-10 14:57:19 +0800

    C++ Primer Plus

---
+++ primer/Ch4/test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char s[10];
+    cin.get(s,10);
+    cout << cin.get();
+    return 0;
+}
---
+++ primer/第三章/test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    unsigned long long b = 99uLL;
+    long long c = -100LL;
+    long long a = b + c;
+    cout << a;
+    return 0;
+}
